5bcd3caac2df154e6fa98d5e7d7ba1486450307f
ChuanTonG.Huang <chuantong.huang@gmail.com>
2016-07-05 09:59:19 +0800

    unittest of busted

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
     gsp(General SQL Parser，http://www.sqlparser.com)
     gsp为收费软件，试用版本只能解释1k个字符长的sql，并限制90天试用。
 
-
-## 编译使用lua 5.1
+## Dev help
+  - 编译使用lua 5.1
+  - 单元测试使用 busted， install by luarocks
---
+++ test/export_func_busted.lua
@@ -0,0 +1,36 @@
+package.cpath = package.cpath.. ";../?.so"
+require("busted")
+
+
+describe("Base export functions and Enums", function()
+        it("functions", function() 
+            gsqlparser = require "gsqlparser"
+
+            assert.are.equal(2 ,  gsqlparser.EVendor.mysql)
+            p = gsqlparser.NewParser(gsqlparser.EVendor.mysql)
+            assert.are.equal(type(gsqlparser.EStmtType), "table")
+            assert.are.equal(type(gsqlparser.ESetType), "table")
+            assert.are.equal(type(gsqlparser.ECreateType), "table")
+            assert.are.equal(type(gsqlparser.EExecType), "table")
+            assert.are.equal(type(gsqlparser.EVendor), "table")
+        
+            query = "SELECT a, b FROM table_1 WHERE c > 20"
+            assert.True(0 == p:check_syntax(query))
+            assert.True(1 == p:get_statement_count())
+            st = p:get_statement(0)
+            assert.True(nil ~= st)
+            node = st:get_root()
+            assert.True(nil ~= node)
+            assert.True(nil ~= getmetatable(node))
+
+            p2 = gsqlparser.NewParser(2)
+            assert.are.equal(getmetatable(p), getmetatable(p2))
+            assert.True(0 == p2:check_syntax(query))
+            st2 = p2:get_statement(0)
+            assert.are.equal(getmetatable(st), getmetatable(st2))
+            n2 = st2:get_root()
+            assert.are_not.equal(getmetatable(node), getmetatable(n2))
+            
+
+        end)
+end)
